b1108c704ba6e50f425505d8994180d3b282bc29
Young Ju Park <parkyngj@gmail.com>
2016-08-04 12:10:59 -0700

    Add pseudocode for REDACTED

--- ruby/redacted.rb
+++ ruby/redacted.rb
@@ -1,1 +1,23 @@
 # A Ruby program that will ask a user for a string (a sentence), ask the user if any words in the sentence should be redacted, and then return the original string with the redacted words removed.
+
+# Ask the user what their sentence is
+puts "What is the sentence you'd like to censor from?"
+sent = gets.chomp
+
+# Ask the user for their desired redacted word
+
+## Execute conversion of sentence:
+# Convert the sentence into an array
+
+# Initialize empty array for new sentence
+
+# Check each element in the array if it's equal to redacted word
+
+  # If it is, then append "REDACTED" to the new array
+
+  # If it isn't, then append the word as it is to the new array
+
+# Now collapse the elements new array into one string/sentence
+
+
+## Conversion complete, return the new string
