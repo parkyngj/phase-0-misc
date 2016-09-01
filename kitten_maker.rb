c366eaa621cee19ca777419a64e76a18c0783b47
Young Ju Park <parkyngj@gmail.com>
2016-09-01 16:26:53 -0700

    Add pseudocode outline to kitten maker file

---
+++ kitten_maker.rb
@@ -0,0 +1,18 @@
+# Operation Kitten Explosion
+# A Ruby program that uses persistent data, involving Object Relational Mapping.
+
+# require gems
+require 'sqlite3'
+require 'faker'
+
+# create SQLite3 database
+
+# learn about fancy string delimiters
+
+# create a kittens table (if it's not there already)
+
+# add a test kitten
+
+# explore ORM by retrieving data
+
+# add lots of kittens
